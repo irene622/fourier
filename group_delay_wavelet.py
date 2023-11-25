a86dd9847399d5e760204f56bc0147f422e049c2
irene622 <dywjd7272@naver.com>
2023-11-25 17:28:31 +0900

    make B

--- group_delay_wavelet.py
+++ group_delay_wavelet.py
@@ -26,3 +26,24 @@ def f_T(x) :
     return x * np.exp(-alpha * x) * np.cos(2*np.pi*f_0*x) * u
 signal = [f_T(x) for x in timepoints]
 
+
+# make matrix B
+def Pi(n, k, T) :
+    if -0.5 <= (n - T*k) / T < 0.5 :
+        return 1
+    else :
+        return 0 
+
+def b_nr(n, r) :
+    b_nr = 0
+    _P = int(P/2)
+    for k in range(-_P, _P) :
+        b_nr += (n - k*T) ^ (2*r) * Pi(n, k, T)
+    return b_nr
+
+B_qT = np.zeros((N, int(R/2 + 1)))
+for n in range(N) :
+    for r in range(int(R/2 + 1)) :
+        B_qT[n][r] = b_nr(int(n/2 + T/2), r)
+print(B_qT.shape)
+print(B_qT[256:260])
