4c59b63e45b82ee9ec2dfdd2f7b16738f5d14db3
irene622 <dywjd7272@naver.com>
2023-11-28 14:05:52 +0900

    modify W. add permutation

--- 03matching_wavelet.py
+++ 03matching_wavelet.py
@@ -48,12 +48,35 @@ def matching_wavelet(signal, N, l, T, delta_omega, P, R, a) :
 
     # Make matrix W
     fft = np.fft.fft(signal)
-    # fft = np.fft.fftshift(fft)
+    fft = np.fft.fftshift(fft)
+    size = fft.shape[0]
+    _permuatation_mat = np.identity(size)
+    permuatation_mat = np.zeros((size, size))
+    n = 0
+    for idx in range(0, size, 2) :
+        permuatation_mat[n, :] = _permuatation_mat[idx, :].copy()
+        n += 1
+    for idx in range(1, size, 2) :
+        permuatation_mat[n] = _permuatation_mat[idx]
+        n += 1
+    print(permuatation_mat)
+    
+    fft = np.matmul(fft, permuatation_mat)
     fft_magnitude = abs(fft)
 
     start_n = int(2**l / 3) + 1
     end_n = int((2**(l+2)) / 3)
+
+    # make matrix W
     W = np.array(fft_magnitude[start_n-1: end_n]) # W.shape = (end_n - start_n + 1, 1)
+    sum_W = sum(W)
+    W = W * 1/sum_W # normalizeing W
+
+    W = np.zeros((end_n - start_n + 1,))
+    n = 0
+    for idx in range(start_n - 1, end_n, delta_omega) : 
+        W[n] = fft_magnitude[idx]
+        n +=1 
     if end_n - start_n + 1 == W.shape[0] :
         print("Right the size of W")
         print(f"W.shape {W.shape} | end_n - start_n {end_n - start_n + 1}")
@@ -62,14 +85,19 @@ def matching_wavelet(signal, N, l, T, delta_omega, P, R, a) :
 
     # Calculate Y
     A_t = np.transpose(A)
-    Y1 = np.matmul(A_t, np.linalg.inv(np.matmul(A, A_t)))
-    Y2 = np.ones(A.shape[0]) - 1/a * np.matmul(A, W)
-    Y = 1/a * W + np.matmul(Y1, Y2)
+    Y1 = np.linalg.inv(np.matmul(A, A_t))
+    Y2 = np.matmul(A_t, Y1)
+    Y3 = np.ones(A.shape[0]) - 1/a * np.matmul(A, W)
+    Y = 1/a * W + np.matmul(Y2, Y3)
+    # sum_Y = sum(Y)
+    # Y = Y * 1/sum_Y
     return Y, W
 
 if __name__ == "__main__" :
     # Matching Discrete Spectrum Amplitude
     Y, W = matching_wavelet(signal, N, l, T, delta_omega, P, R, a)
+    # W = np.sqrt(W)
+    Y = np.sqrt(Y)
 
     # Draw the vector Y
     plt.title("Amplitude matching")
@@ -78,4 +106,4 @@ if __name__ == "__main__" :
     plt.grid()
     plt.legend()
 
-    plt.savefig("Amplitude_matching.png")
+    plt.savefig("03amplitude_matching_wavelet.png")
