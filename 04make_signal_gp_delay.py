da2a91dcc470cd17d8adaaf9978d20af30c3938a
irene622 <dywjd7272@naver.com>
2023-12-07 15:50:25 +0900

    below 0.001

--- 04make_signal_gp_delay.py
+++ 04make_signal_gp_delay.py
@@ -28,27 +28,32 @@ signal = [f_T(x) for x in timepoints]
 fft = np.fft.fft(signal)
 fft = np.fft.fftshift(fft)
 fft_magnitude = abs(fft)
-fft_magnitude = fft_magnitude[:int(N/2)]
-print(min(fft_magnitude))
-print(max(fft_magnitude))
-num=0
-for i in fft_magnitude :
-    if i <= 10**(-2) :
-        num += 1
-print(num)
-
+fft_magnitude = fft_magnitude[:int(N/2)] # 0.007853076233206524, 3.8579644884021937
 
 T = N / sampling_freq
 k = np.arange(N)
 freq = k / T
 freq = freq[:int(N/2)]
-f = interpolate.interp1d(freq, fft_magnitude, kind = 'quadratic')
+
+data = []
+for i in range(int(N/2)) :
+    data.append((fft_magnitude[i], freq[i]))
+
+# filter the magnitude <= 10**-2
+filtered_freq = []
+filtered_fft_magnitude = []
+for fft_mag, freq in data :
+    if fft_mag <= 10**(-2) :
+        filtered_fft_magnitude.append(fft_mag)
+        filtered_freq.append(freq)
+
+f = interpolate.interp1d(filtered_freq, filtered_fft_magnitude, kind = 'quadratic')
 
 # Draw the signal graph
 plt.figure(figsize=(7,5))
 plt.title("f")
 
-plt.plot(freq, f(freq))
+plt.plot(freq, f(filtered_freq))
 plt.grid()
 
 plt.savefig("04group_delay_signal.png")
